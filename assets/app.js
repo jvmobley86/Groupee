f5d2f30cb9610d5d43d14158fc2909546e86ed37
Bryan McCann <bmac929@yahoo.com>
2019-06-25 18:36:53 -0500

    spotiy api

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,6 @@
 var artist = "ShawnMendes"; //need to change this out to an array later on [];
 var ticketApikey = "eyAxFU6bJI5DbKGIOKnrAd38BP42m5kf"
-var queryURL = `https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=${ticketApikey}&keyword=` + artist;
-
+var queryURL = `https://app.ticketmaster.com/discovery/v2/attractions/.json?apikey=${ticketApikey}&keyword=` + artist;
 //string literals(new laguage to look up)
 
 
@@ -13,10 +12,11 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-    console.log(response)
+    console.log(response.data);
     // var artistId = response._embedded.attractions[0].id
 
 });
+
 
 // trim venue information to cities near Dallas
 
